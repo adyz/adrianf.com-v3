16bc4390ae2a1e1969e52cf6f72a2764b283d2ee
Adrian Florescu <florescuadyz@yahoo.com>
2021-12-19 15:28:20 +0200

    Removed theme color

--- app/root.tsx
+++ app/root.tsx
@@ -93,7 +93,6 @@ function Document({
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="theme-color" content="#000" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         {title ? <title>{title}</title> : null}
         <Meta />
